4e4902990f79f293c46dbf2caebbb21df8c6bcf5
WillDeveloping <williamjsharland@icloud.com>
2026-01-05 16:21:18 +0000

    updating cpp

---
+++ ConsoleApplication1/ConsoleApplication1/Monster.cpp
@@ -0,0 +1,32 @@
+#include "Monster.h"
+#include <iostream>
+
+Monster::Monster()
+{
+}
+
+Monster::Monster(string nName, string nType, int attackStat)
+{
+	
+}
+
+string Monster::getName()
+{
+	return string();
+}
+
+string Monster::getType()
+{
+	return string();
+}
+
+int Monster::gethealth()
+{
+	return 0;
+}
+
+int Monster::getattackStat()
+{
+	return 0;
+}
+
